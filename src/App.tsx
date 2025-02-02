7d9cba2ecacb96e1818ba0fe21234bfa965cf7bc
qiuxan <qiuxan@gmail.com>
2025-02-02 19:12:09 +1100

    Move it to the use effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,7 +7,11 @@ function App() {
 	const [count, setCount] = useState(0);
 
   // this is a side effect because document is not a part of react and this function
-  document.title = `count: ${count}`;
+  // document.title = `count: ${count}`;
+
+  useEffect(() => {
+    document.title = `count: ${count}`;
+  });
 
 	return <>
     <div>
