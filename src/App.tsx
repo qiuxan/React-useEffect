6e2fef00ffda360d06aeefed456d675b3f562e75
qiuxan <qiuxan@gmail.com>
2025-02-02 18:34:01 +1100

    A working but not so good side effect example

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
+  // this is a side effect because document is not a part of react and this function
+  document.title = `count: ${count}`;
 
-    </>
-  )
+	return <>
+    <div>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  </>;
 }
 
-export default App
+export default App;
